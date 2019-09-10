e1138fa965628c90dd199b45a879673fafb46c6c
gregorioospina <g.ospinaa@uniandes.edu.co>
2019-09-10 15:40:43 -0500

    Chao chao

--- app/backend/server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`);
-});
