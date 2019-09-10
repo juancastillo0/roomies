a9a1149c835e062f748b0787804a2f6deac917a2
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-09-10 17:08:10 -0500

    "MERN stack config completed"

--- app/roomies-backend/routes/index.js
+++ app/roomies-backend/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+const uri = process.env.ATLAS_URI;
+const client = new MongoClient(uri);
+
+//Connection to MongoDB Atlas.
+const getMovies = (resolve) => {
+  client
+    .connect(function (err, client) {
+      if (err) {
+        console.log('Error occurred while connecting to MongoDB Atlas...\n', err);
+      }
+
+      console.log('Connected to mongo atlas...');
+
+      const db = client.db('sample_mflix');
+      const movies = db.collection('movies');
+
+      movies.find({})
+        .limit(20)
+        .toArray((err, data) => {
+          if (err) throw err;
+
+          resolve(data);
+          client.close();
+        })
+    });
+};
+
 /* GET home page. */
-router.get('/data', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/data', function (req, res, next) {
+  getMovies((data) => {
+    res.json(data);
+  })
 });
 
 module.exports = router;
