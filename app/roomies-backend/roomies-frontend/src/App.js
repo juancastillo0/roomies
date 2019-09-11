d3406926669107c22d70d8b2336c4891b7c5a2fb
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-09-11 11:27:27 -0500

    Mongo connection tryout

--- app/roomies-backend/roomies-frontend/src/App.js
+++ app/roomies-backend/roomies-frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Menu from "./components/Menu";
 import MessageBoard from "./components/MessageBoard";
@@ -6,30 +6,32 @@ import Vaca from "./components/Vaca";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div>
-      <header>
-        <Navbar />
-      </header>
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <Navbar />
+        </header>
 
-      <main>
-        <div class="container-fluid" id="homecontainer">
-          <div class="row">
-            <div class="col-3">
-              <Menu />
-            </div>
-            <div class="col-6" id="messageboard">
-              <MessageBoard />
-            </div>
-            <div class="col-3">
-              <Vaca />
+        <main>
+          <div class="container-fluid" id="homecontainer">
+            <div class="row">
+              <div class="col-3">
+                <Menu />
+              </div>
+              <div class="col-6" id="messageboard">
+                <MessageBoard />
+              </div>
+              <div class="col-3">
+                <Vaca />
+              </div>
             </div>
           </div>
-        </div>
-      </main>
-    </div>
-  );
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
--- app/roomies-backend/roomies-frontend/src/components/MessageBoard.js
+++ app/roomies-backend/roomies-frontend/src/components/MessageBoard.js
@@ -1,9 +1,31 @@
 import React, { Component } from "react";
 
 class MessageBoard extends Component {
-  state = {};
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      movies: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("/data")
+      .then(res => res.json())
+      .then(movies => this.setState({ movies: movies }));
+  }
+
+  renderTest() {
+    return this.state.movies.map(m => <h3>{m.title}</h3>);
+  }
+
   render() {
-    return <h1>Aqui va el message board</h1>;
+    return (
+      <div>
+        <h1>Aqui va el message board</h1>
+        {this.renderTest()}
+      </div>
+    );
   }
 }
 
