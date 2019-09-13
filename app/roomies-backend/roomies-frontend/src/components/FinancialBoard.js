3dcfc90944b7b9abe9c7bc25f7f483ea9d9c3d53
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-09-12 21:03:06 -0500

    Financial board changes

--- app/roomies-backend/roomies-frontend/src/components/FinancialBoard.js
+++ app/roomies-backend/roomies-frontend/src/components/FinancialBoard.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 import "./FinancialBoard.css";
+import Clock from "../images/clock.png";
 
 class FinancialBoard extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      debt: 400000
+      debt: 400000,
+      bills: [
+        {
+          name: "bill",
+          value: 0
+        }
+      ]
     };
   }
 
@@ -21,9 +28,19 @@ class FinancialBoard extends Component {
           <div className="card-body">
             <h3 className="card-title">Debt</h3>
             <h1 className="card-text" id="debt">
-              {this.state.debt.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
+              $ {this.state.debt.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,")}
             </h1>
           </div>
+        </div>
+        <div id="bills-container">
+          <ul class="list-group">
+            <li class="list-group-item">
+              <div className="row">
+                <img src={Clock} id="bill-status" />
+                <div className="col">Water ($5000)</div>
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     );
