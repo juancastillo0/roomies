9406b687728376e65547e7904930df3b185635ad
snaranjop1 <s.naranjop1@uniandes.edu.co>
2019-09-13 12:42:35 -0500

    Menu ready

--- app/roomies-backend/roomies-frontend/src/components/Menu.js
+++ app/roomies-backend/roomies-frontend/src/components/Menu.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import Home from "../images/home.png";
+import Financial from "../images/financial.png";
+import Todo from "../images/todo.png";
 import "./Menu.css";
 
 class Menu extends Component {
@@ -6,7 +9,57 @@ class Menu extends Component {
   render() {
     return (
       <div id="menu-container">
-        <h1>Aqui va el menu</h1>
+        <div class="row">
+          <div class="col-4">
+            <div class="list-group" id="list-tab" role="tablist">
+              <a
+                class="list-group-item list-group-item-action active"
+                id="list-home-list"
+                data-toggle="list"
+                href="#home"
+                role="tab"
+                aria-controls="home"
+              >
+                <div className="row">
+                  <div className="col-2">
+                    <img src={Home} className="" alt="" id="home-icon" />
+                  </div>
+                  <div className="col">Home</div>
+                </div>
+              </a>
+              <a
+                class="list-group-item list-group-item-action"
+                id="list-home-list"
+                data-toggle="list"
+                href="#filter-financial"
+                role="tab"
+                aria-controls="home"
+              >
+                <div className="row">
+                  <div className="col-2">
+                    <img src={Financial} className="" alt="" id="home-icon" />
+                  </div>
+                  <div className="col">Financial</div>
+                </div>
+              </a>
+              <a
+                class="list-group-item list-group-item-action"
+                id="list-home-list"
+                data-toggle="list"
+                href="#list-home"
+                role="tab"
+                aria-controls="home"
+              >
+                <div className="row">
+                  <div className="col-2">
+                    <img src={Todo} className="" alt="" id="home-icon" />
+                  </div>
+                  <div className="col">Todo's</div>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
